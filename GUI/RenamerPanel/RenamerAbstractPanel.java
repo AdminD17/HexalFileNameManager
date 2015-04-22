4285e965c74f13a3a268bf99372a56b77a0feff6
AdminD17 <davidgiordana0@gmail.com>
2015-04-22 09:55:26 -0300

    incorporación de panel de renombre
    el panel es abstracto para la creación de paneles de renombre

---
+++ GUI/RenamerPanel/RenamerAbstractPanel.java
@@ -0,0 +1,26 @@
+package HexalFileNameManager.GUI.RenamerPanel;
+
+import javax.swing.JPanel;
+
+/**
+ * Clase abstracta que extiende de un @See JPanel que contiene los
+ * metodos y atributos necesarios para renombrar archivos
+ * 
+ * @author David Giordana
+ *
+ */
+public abstract class RenamerAbstractPanel extends JPanel {
+
+	private static final long serialVersionUID = 2337765671710528056L;
+	
+	/**
+	 * ---- ABSTRACT METHODS
+	 */
+	
+	/**
+	 * renombra los archivos de una tabla
+	 */
+	public abstract void rename();
+	
+	
+}
